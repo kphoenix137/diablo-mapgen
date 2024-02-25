c2b71b2752bced7c94a1b5f0f455e999ec4e1b8a
staphen <staphen@gmail.com>
2024-02-24 20:18:07 -0500

    Generate items from monsters and objects

--- Source/funkMapGen.cpp
+++ Source/funkMapGen.cpp
@@ -283,6 +283,102 @@ void seedSelection(uint32_t seed)
 	}
 }
 
+void createItemsFromObject(int oid)
+{
+	switch (object[oid]._otype) {
+	case OBJ_CHEST1:
+	case OBJ_CHEST2:
+	case OBJ_CHEST3:
+	case OBJ_TCHEST1:
+	case OBJ_TCHEST2:
+	case OBJ_TCHEST3:
+		SetRndSeed(object[oid]._oRndSeed);
+		if (setlevel) {
+			for (int j = 0; j < object[oid]._oVar1; j++) {
+				CreateRndItem(object[oid]._ox, object[oid]._oy, TRUE, TRUE, FALSE);
+			}
+		} else {
+			for (int j = 0; j < object[oid]._oVar1; j++) {
+				if (object[oid]._oVar2 != 0)
+					CreateRndItem(object[oid]._ox, object[oid]._oy, FALSE, TRUE, FALSE);
+				else
+					CreateRndUseful(0, object[oid]._ox, object[oid]._oy, TRUE);
+			}
+		}
+		break;
+	case OBJ_SARC:
+		SetRndSeed(object[oid]._oRndSeed);
+		if (object[oid]._oVar1 <= 2)
+			CreateRndItem(object[oid]._ox, object[oid]._oy, FALSE, TRUE, FALSE);
+		break;
+	case OBJ_DECAP:
+		SetRndSeed(object[oid]._oRndSeed);
+		CreateRndItem(object[oid]._ox, object[oid]._oy, FALSE, TRUE, FALSE);
+		break;
+	case OBJ_BARREL:
+		SetRndSeed(object[oid]._oRndSeed);
+		if (object[oid]._oVar2 <= 1) {
+			if (object[oid]._oVar3 == 0)
+				CreateRndUseful(0, object[oid]._ox, object[oid]._oy, TRUE);
+			else
+				CreateRndItem(object[oid]._ox, object[oid]._oy, FALSE, TRUE, FALSE);
+		}
+		break;
+	case OBJ_SKELBOOK:
+	case OBJ_BOOKSTAND:
+		SetRndSeed(object[oid]._oRndSeed);
+		if (random_(161, 5) != 0)
+			CreateTypeItem(object[oid]._ox, object[oid]._oy, FALSE, ITYPE_MISC, IMISC_SCROLL, TRUE, FALSE);
+		else
+			CreateTypeItem(object[oid]._ox, object[oid]._oy, FALSE, ITYPE_MISC, IMISC_BOOK, TRUE, FALSE);
+		break;
+	case OBJ_BOOKCASEL:
+	case OBJ_BOOKCASER:
+		SetRndSeed(object[oid]._oRndSeed);
+		CreateTypeItem(object[oid]._ox, object[oid]._oy, FALSE, ITYPE_MISC, IMISC_BOOK, TRUE, FALSE);
+		break;
+	case OBJ_ARMORSTAND:
+	case OBJ_WARARMOR: {
+		SetRndSeed(object[oid]._oRndSeed);
+		BOOL uniqueRnd = random_(0, 2);
+		if (currlevel <= 5) {
+			CreateTypeItem(object[oid]._ox, object[oid]._oy, TRUE, ITYPE_LARMOR, IMISC_NONE, TRUE, FALSE);
+		} else if (currlevel >= 6 && currlevel <= 9) {
+			CreateTypeItem(object[oid]._ox, object[oid]._oy, uniqueRnd, ITYPE_MARMOR, IMISC_NONE, TRUE, FALSE);
+		} else if (currlevel >= 10 && currlevel <= 12) {
+			CreateTypeItem(object[oid]._ox, object[oid]._oy, FALSE, ITYPE_HARMOR, IMISC_NONE, TRUE, FALSE);
+		} else if (currlevel >= 13 && currlevel <= 16) {
+			CreateTypeItem(object[oid]._ox, object[oid]._oy, TRUE, ITYPE_HARMOR, IMISC_NONE, TRUE, FALSE);
+		}
+	} break;
+	case OBJ_WARWEAP:
+	case OBJ_WEAPONRACK: {
+		SetRndSeed(object[oid]._oRndSeed);
+		int weaponType;
+
+		switch (random_(0, 4) + ITYPE_SWORD) {
+		case ITYPE_SWORD:
+			weaponType = ITYPE_SWORD;
+			break;
+		case ITYPE_AXE:
+			weaponType = ITYPE_AXE;
+			break;
+		case ITYPE_BOW:
+			weaponType = ITYPE_BOW;
+			break;
+		case ITYPE_MACE:
+			weaponType = ITYPE_MACE;
+			break;
+		}
+
+		if (leveltype > 1)
+			CreateTypeItem(object[oid]._ox, object[oid]._oy, TRUE, weaponType, IMISC_NONE, TRUE, FALSE);
+		else
+			CreateTypeItem(object[oid]._ox, object[oid]._oy, FALSE, weaponType, IMISC_NONE, TRUE, FALSE);
+	} break;
+	}
+}
+
 void printAsciiLevel()
 {
 	bool steps[MAXDUNX][MAXDUNY];
@@ -420,6 +516,24 @@ int main(int argc, char **argv)
 				std::cout << "Item Count: " << numitems << std::endl;
 				for (int i = 0; i < numitems; i++) {
 					std::cout << "Item " << i << ": " << item[itemactive[i]]._iIName << " (" << item[itemactive[i]]._iSeed << ")" << std::endl;
+				}
+
+				std::cout << std::endl;
+				for (int i = MAX_PLRS; i < nummonsters; i++) {
+					int ii = numitems;
+					int mid = monstactive[i];
+					SetRndSeed(monster[mid]._mRndSeed);
+					SpawnItem(mid, monster[mid]._mx, monster[mid]._my, TRUE);
+					if (numitems > ii)
+						std::cout << "Monster Item " << ii << ": " << item[itemactive[ii]]._iIName << " (" << item[itemactive[ii]]._iSeed << ")" << std::endl;
+				}
+				std::cout << std::endl;
+				for (int i = 0; i < nobjects; i++) {
+					int ii = numitems;
+					int oid = objectactive[i];
+					createItemsFromObject(oid);
+					for (int j = ii; j < numitems; j++)
+						std::cout << "Object Item " << j << ": " << item[itemactive[j]]._iIName << " (" << item[itemactive[j]]._iSeed << ")" << std::endl;
 				}
 			}
 			if (exportLevels)
