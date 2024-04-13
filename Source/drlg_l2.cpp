14e18e787c3d19034c55969236cc8898660683c2
Anders Jenbo <anders@jenbo.dk>
2024-04-14 00:33:48 +0200

    Revert "Apply reroll fix"
    This reverts commit b26dd59861627cbf82dbf86444fa5c385f5a5099.

--- Source/drlg_l2.cpp
+++ Source/drlg_l2.cpp
@@ -3515,10 +3515,24 @@ void LoadPreL2Dungeon(const char *sFileName, int vx, int vy)
 
 std::optional<uint32_t> CreateL2Dungeon(DWORD rseed, int entry, DungeonMode mode)
 {
-	nSx1 = -1;
-	nSy1 = -1;
-	nSx2 = -1;
-	nSy2 = -1;
+	if (gbMaxPlayers == 1) {
+		if (currlevel == 7 && quests[Q_BLIND]._qactive == QUEST_NOTAVAIL) {
+			currlevel = 6;
+			CreateL2Dungeon(glSeedTbl[6], 4, breakOnSuccess);
+			currlevel = 7;
+		}
+		if (currlevel == 8) {
+			if (quests[Q_BLIND]._qactive == QUEST_NOTAVAIL) {
+				currlevel = 6;
+				CreateL2Dungeon(glSeedTbl[6], 4, breakOnSuccess);
+				currlevel = 8;
+			} else {
+				currlevel = 7;
+				CreateL2Dungeon(glSeedTbl[7], 4, breakOnSuccess);
+				currlevel = 8;
+			}
+		}
+	}
 
 	SetRndSeed(rseed);
 
