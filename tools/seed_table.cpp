f9cc60afc671aceadf378d3020164a00424847da
Andrew James <ephphatha@thelettereph.com>
2024-04-13 10:18:56 +1000

    apply format rules

--- tools/seed_table.cpp
+++ tools/seed_table.cpp
@@ -9,13 +9,15 @@
 #include <optional>
 #include <string_view>
 
-static void showUsage(std::string_view programName) {
+static void showUsage(std::string_view programName)
+{
 	std::cout << "Usage: " << programName << " <level> <dungeon seed>\n";
 	std::cout << "       " << programName << " <game seed>\n";
 }
 
-template<typename resultType, typename intermediateType>
-std::optional<resultType> parseNumber(std::string_view numericString, intermediateType minValue, intermediateType maxValue) {
+template <typename resultType, typename intermediateType>
+std::optional<resultType> parseNumber(std::string_view numericString, intermediateType minValue, intermediateType maxValue)
+{
 	intermediateType value = 0;
 	switch (std::from_chars(numericString.data(), numericString.data() + numericString.size(), value).ec) {
 	case std::errc():
@@ -23,6 +25,7 @@ std::optional<resultType> parseNumber(std::string_view numericString, intermedia
 			return static_cast<resultType>(value);
 		}
 		// else fall through
+		[[fallthrough]];
 	case std::errc::result_out_of_range:
 		std::cout << numericString << " is outside the expected range of " << minValue << " to " << maxValue << ".\n";
 		break;
@@ -36,52 +39,57 @@ std::optional<resultType> parseNumber(std::string_view numericString, intermedia
 	return {};
 }
 
-static std::optional<uint8_t> parseLevel(std::string_view numericString) {
+static std::optional<uint8_t> parseLevel(std::string_view numericString)
+{
 	return parseNumber<uint8_t, int8_t>(numericString, 0, 16);
 }
 
-static std::optional<uint32_t> parseSeed(std::string_view numericString) {
+static std::optional<uint32_t> parseSeed(std::string_view numericString)
+{
 	return parseNumber<uint32_t, int64_t>(numericString, std::numeric_limits<int32_t>::min(), std::numeric_limits<uint32_t>::max());
 }
 
-constexpr uint32_t Multiplier = 22695477; // 0x015A4E35;
-constexpr uint32_t Increment = 1;
+constexpr uint32_t Multiplier        = 22695477; // 0x015A4E35;
+constexpr uint32_t Increment         = 1;
 constexpr uint32_t InverseMultiplier = 690295837; // computed using PowerMod[22695477,-1,2^32] in Wolfram Playground
 
-static uint32_t advanceRng(uint32_t state) {
+static uint32_t advanceRng(uint32_t state)
+{
 	return state * Multiplier + Increment;
 }
 
-static uint32_t backtrackRng(uint32_t state) {
+static uint32_t backtrackRng(uint32_t state)
+{
 	return (state - Increment) * InverseMultiplier;
 }
 
 struct Quests {
-	bool butcher = true;
-	bool pwater = true;
+	bool butcher  = true;
+	bool pwater   = true;
 	bool skelking = true;
 	bool ltbanner = true;
-	bool blood = true;
-	bool schamb = true;
-	bool blind = true;
-	bool anvil = true;
-	bool warlord = true;
+	bool blood    = true;
+	bool schamb   = true;
+	bool blind    = true;
+	bool anvil    = true;
+	bool warlord  = true;
 };
 
-static int32_t absShiftMod(uint32_t state, int32_t limit) {
+static int32_t absShiftMod(uint32_t state, int32_t limit)
+{
 	int32_t seed = static_cast<int32_t>(state);
-	seed = seed == std::numeric_limits<int32_t>::min() ? std::numeric_limits<int32_t>::min() : std::abs(seed);
+	seed         = seed == std::numeric_limits<int32_t>::min() ? std::numeric_limits<int32_t>::min() : std::abs(seed);
 	return (seed >> 16) % limit;
 }
 
-static Quests determineActiveQuests(uint32_t seed) {
-	Quests activeQuests{};
+static Quests determineActiveQuests(uint32_t seed)
+{
+	Quests activeQuests {};
 
 	uint32_t state = advanceRng(seed);
 	if (absShiftMod(state, 2) == 0) {
 		activeQuests.skelking = false;
-	}
-	else {
+	} else {
 		activeQuests.pwater = false;
 	}
 
@@ -95,7 +103,7 @@ static Quests determineActiveQuests(uint32_t seed) {
 		break;
 	case 2:
 		// Gharbad the Weak doesn't impact map generation
-		//activeQuests.garbund = false;
+		// activeQuests.garbund = false;
 		break;
 	case std::numeric_limits<int32_t>::min():
 	default:
@@ -110,7 +118,7 @@ static Quests determineActiveQuests(uint32_t seed) {
 		break;
 	case 1:
 		// The Magic Rock doesn't impact map generation
-		//activeQuests.rock = false;
+		// activeQuests.rock = false;
 		break;
 	case 2:
 		activeQuests.blood = false;
@@ -125,11 +133,11 @@ static Quests determineActiveQuests(uint32_t seed) {
 	switch (absShiftMod(state, 3)) {
 	case 0:
 		// Black Mushroom doesn't impact map generation
-		//activeQuests.mushroom = false;
+		// activeQuests.mushroom = false;
 		break;
 	case 1:
 		// Zhar the Mad doesn't impact map generation
-		//activeQuests.zhar = false;
+		// activeQuests.zhar = false;
 		break;
 	case 2:
 		activeQuests.anvil = false;
@@ -142,9 +150,8 @@ static Quests determineActiveQuests(uint32_t seed) {
 
 	if (absShiftMod(advanceRng(state), 2) == 0) {
 		// Lachdanan doesn't impact map generation
-		//activeQuests.veil = false;
-	}
-	else {
+		// activeQuests.veil = false;
+	} else {
 		activeQuests.warlord = false;
 	}
 
@@ -154,46 +161,48 @@ static Quests determineActiveQuests(uint32_t seed) {
 struct GameState {
 	uint32_t startingSeed;
 
-	std::array<int32_t, 17> seedTable{};
+	std::array<int32_t, 17> seedTable {};
 
 	GameState() = delete;
 
-	GameState(uint32_t state) : startingSeed(state) {
-		for (auto& dungeonSeed : seedTable) {
-			state = advanceRng(state);
+	GameState(uint32_t state)
+	    : startingSeed(state)
+	{
+		for (auto &dungeonSeed : seedTable) {
+			state       = advanceRng(state);
 			dungeonSeed = std::abs(static_cast<int32_t>(state));
 		}
 	}
 
-	GameState(uint8_t level, uint32_t seed, bool useNegatedState = false) {
+	GameState(uint8_t level, uint32_t seed, bool useNegatedState = false)
+	{
 		seedTable[level] = seed;
 
 		uint32_t state = useNegatedState ? -static_cast<int32_t>(seed) : seed;
 		for (int i = level - 1; i >= 0; --i) {
-			state = backtrackRng(state);
+			state        = backtrackRng(state);
 			seedTable[i] = std::abs(static_cast<int32_t>(state));
 		}
 		startingSeed = backtrackRng(state);
 
 		state = useNegatedState ? -static_cast<int32_t>(seed) : seed;
 		for (int i = level + 1; i < seedTable.size(); ++i) {
-			state = advanceRng(state);
+			state        = advanceRng(state);
 			seedTable[i] = std::abs(static_cast<int32_t>(state));
 		}
 	}
 };
 
-static void renderSeedTable(const GameState &state) {
+static void renderSeedTable(const GameState &state)
+{
 
 	time_t startingTime = std::chrono::system_clock::to_time_t(std::chrono::time_point<std::chrono::system_clock>(std::chrono::seconds(state.startingSeed)));
 	std::cout << "Game seed: " << state.startingSeed << " (" << std::put_time(std::gmtime(&startingTime), "%Y-%m-%d %H:%M:%S");
 	if (315532800 <= state.startingSeed && state.startingSeed <= 2177452799U) {
 		std::cout << " can be reached by setting the system time)\n";
-	}
-	else if (state.startingSeed < 315532800) {
+	} else if (state.startingSeed < 315532800) {
 		std::cout << " requires an NT based Windows and an old BIOS or using the Mac version)\n";
-	}
-	else {
+	} else {
 		std::cout << " requires a modern recompile)\n";
 	}
 
@@ -204,40 +213,32 @@ static void renderSeedTable(const GameState &state) {
 		if (i == 2) {
 			if (activeQuests.butcher && activeQuests.pwater) {
 				std::cout << " (with The Butcher and Poison Water Supply [Q_BUTCHER && Q_PWATER])";
-			}
-			else if (activeQuests.butcher) {
+			} else if (activeQuests.butcher) {
 				std::cout << " (with The Butcher [Q_BUTCHER])";
-			}
-			else if (activeQuests.pwater) {
+			} else if (activeQuests.pwater) {
 				std::cout << " (with Poisoned Water Supply [Q_PWATER])";
 			}
-		}
-		else if (i == 3 && activeQuests.skelking) {
+		} else if (i == 3 && activeQuests.skelking) {
 			std::cout << " (with The Curse of King Leoric [Q_SKELKING])";
-		}
-		else if (i == 4 && activeQuests.ltbanner) {
+		} else if (i == 4 && activeQuests.ltbanner) {
 			std::cout << " (with Ogden's Sign [Q_LTBANNER])";
-		}
-		else if (i == 5 && activeQuests.blood) {
+		} else if (i == 5 && activeQuests.blood) {
 			std::cout << " (with Valor [Q_BLOOD])";
-		}
-		else if (i == 6 && activeQuests.schamb) {
+		} else if (i == 6 && activeQuests.schamb) {
 			std::cout << " (with Chamber of Bone [Q_SCHAMB])";
-		}
-		else if (i == 7 && activeQuests.blind) {
+		} else if (i == 7 && activeQuests.blind) {
 			std::cout << " (with Halls of the Blind [Q_BLIND])";
-		}
-		else if (i == 10 && activeQuests.anvil) {
+		} else if (i == 10 && activeQuests.anvil) {
 			std::cout << " (with Anvil of Fury [Q_ANVIL])";
-		}
-		else if (i == 13 && activeQuests.warlord) {
+		} else if (i == 13 && activeQuests.warlord) {
 			std::cout << " (with Warlord of Blood [Q_WARLORD])";
 		}
 		std::cout << "\n";
 	}
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
 	if (argc < 2) {
 		showUsage(argv[0]);
 		return 1;
@@ -257,7 +258,7 @@ int main(int argc, char* argv[]) {
 
 	std::optional<uint32_t> seed = parseSeed(*++argv);
 	if (!seed) {
-		std::cerr << "Could not parse " << (level ? "dungeon" : "game") << "seed.\n";
+		std::cerr << "Could not parse " << (level ? "dungeon" : "game") << " seed.\n";
 		return 1;
 	}
 
@@ -270,11 +271,9 @@ int main(int argc, char* argv[]) {
 			std::cout << "\nDungeon seed could have been naturally generated from a negated state, alternative table follows:\n";
 			state = GameState(*level, *seed, true);
 			renderSeedTable(state);
-		}
-		else if (*seed == 0 || *seed == static_cast<uint32_t>(std::numeric_limits<int32_t>::min())) {
+		} else if (*seed == 0 || *seed == static_cast<uint32_t>(std::numeric_limits<int32_t>::min())) {
 			std::cout << "Only one state could lead to that dungeon seed.\n";
-		}
-		else {
+		} else {
 			std::cout << "This dungeon seed can only be used with a modified save.\n";
 		}
 	}
