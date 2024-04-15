8763d19caa2721c827abb4cf2112ce08c53d7eb0
Anders Jenbo <anders@jenbo.dk>
2024-04-15 05:30:26 +0200

    Properly fix % calcuation

--- Source/funkMapGen.cpp
+++ Source/funkMapGen.cpp
@@ -252,7 +252,7 @@ void printProgress(uint32_t seedIndex, uint32_t seed)
 		return;
 	ProgressseedMicros += elapsed;
 
-	int pct = seedIndex / Config.seedCount * 100;
+	uint64_t pct = 100 * seedIndex / Config.seedCount;
 	int speed = (seedIndex - ProgressseedIndex) / 10;
 	int seconds = (Config.seedCount - seedIndex) / speed;
 	ProgressseedIndex = seedIndex;
